793f6e63d94ed25d2102473b8b5ca1a661637934
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-07-22 23:03:31 +0100

    Implemented home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './main.css'
+import reactLogo from './assets/react.svg'
 import MenuBar from './components/MenuBar'
+import HomePage from './components/HomePage'
 
 function App() {
   return (
     <>
       <MenuBar />
+      <HomePage />
+      <footer>Developed by Fahim Ahmed <img src={reactLogo} /></footer>
     </>
   )
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <h1>Welcome to The One Shop</h1>
+            <p>We are definitely not One Stop</p>
+            <p>We are a new store with many different products</p>
+            <p>Check out our brand new products</p>
+            <button type="button" className="shop">Shop with us now</button>
+        </div>
+    );
+};
+
+export default HomePage;
